3f46baf044eb31d94195012bcbf1a136413baa6c
Maurice <mauricenganga41@gmail.com>
2022-09-03 13:58:39 +0300

    Add routing to the database to help in crud operations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, "application/json"
 
-    gets "/" do
+    get "/notes" do
         notes = Note.all.order(:created_at)
         notes.to_json
+    end
+
+    post "/notes" do
+        note = Note.create(
+            title:params[:title],
+            content:params[:content]
+        )
+        note.to_json
+    end
+
+    delete "/notes/:id" do 
+        note = Note.find(params[:id])
+        note.destroy
+        note.to_json
     end
 end
