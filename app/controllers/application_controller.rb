1d48a41f4a25d63c38b5bb76fed1277ae39647ca
Maurice <mauricenganga41@gmail.com>
2022-09-03 13:02:06 +0300

    Create migration to make a notes table with two columns of title and content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < Sinatra::Base
+    set :default_content_type, "application/json"
+
+    gets "/" do
+        notes = Note.all.order(:created_at)
+        notes.to_json
+    end
 end
