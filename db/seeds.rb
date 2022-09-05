24b59fa607820d684040e6bb9f8f61f31a79f63e
Maurice <mauricenganga41@gmail.com>
2022-09-05 07:14:26 +0300

    Create a seed file to for testing data

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+puts "Seeding notes..."
+Note.create(title: "Group one project", content: "Group one decided on note app web application. Front-end build using React while the backend using Active Record and Sinatra")
+Note.create(title: "Urg Monday", content: "The first 5 days after the weekend are always the hardest")
+
+puts "Done seeding!"
